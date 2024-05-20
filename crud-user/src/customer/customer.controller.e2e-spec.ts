9aa3eefd6a3acaef07d93d12e6f9a2e966d409d5
Agner Souza Bezerra <agner.bezerra@toledobrasil.com>
2024-05-20 16:27:55 -0300

    Ajusta os testes da rota customers para ficarem protegidas.

--- crud-user/src/customer/customer.controller.e2e-spec.ts
+++ crud-user/src/customer/customer.controller.e2e-spec.ts
@@ -5,9 +5,25 @@ const baseURL = process.env.API_URL;
 describe('Test the customer controller (e2e)', () => {
   const apiRequest = request(baseURL);
   let idUserCreated = null;
+  let accessToken = null;
+  it('should do a login with user and password and the response must be equal 200', async () => {
+    const response = await apiRequest
+      .post('/auth/login')
+      .send({
+        userEmail: 'agner.functional.test@test.com',
+        userPassword: '123456',
+      })
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('access_token');
+    accessToken = response?.body?.access_token;
+  });
 
   it('should create a new customer without image and data body and the response must be equal 400', async () => {
-    const response = await apiRequest.post('/customer');
+    const response = await apiRequest
+      .post('/customer')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(400);
     expect(response.body).toMatchObject({
       message: 'customerImage field is required',
@@ -19,7 +35,8 @@ describe('Test the customer controller (e2e)', () => {
   it('should create a new customer with image but without data body and the response must be equal 400', async () => {
     const response = await apiRequest
       .post('/customer')
-      .attach('customerImage', './test/image/cachorrinhos-filhotes.jpg');
+      .attach('customerImage', './test/image/cachorrinhos-filhotes.jpg')
+      .set('Authorization', `Bearer ${accessToken}`);
 
     expect(response.status).toBe(400);
     expect(response.body).toMatchObject({
@@ -41,14 +58,17 @@ describe('Test the customer controller (e2e)', () => {
         customerEmail: 'agner.functional.test@test.com',
         status: 'active',
       })
-      .attach('customerImage', './test/image/cachorrinhos-filhotes.jpg');
+      .attach('customerImage', './test/image/cachorrinhos-filhotes.jpg')
+      .set('Authorization', `Bearer ${accessToken}`);
 
     expect(response.status).toBe(201);
     idUserCreated = response?.body?.id;
   });
 
   it('should search a specific customer and the response be equal 200 and equal specifics object propertys', async () => {
-    const response = await apiRequest.get(`/customer/${idUserCreated}`);
+    const response = await apiRequest
+      .get(`/customer/${idUserCreated}`)
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({
       id: idUserCreated,
@@ -63,7 +83,9 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should update a specific customer and the response be equal 404 and recive the message customer not found', async () => {
-    const response = await apiRequest.put('/customer/999999999999999');
+    const response = await apiRequest
+      .put('/customer/999999999999999')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
       message: 'Customer with id 999999999999999 not found',
@@ -71,15 +93,20 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should update a specific customer and the response be equal 200', async () => {
-    const response = await apiRequest.put(`/customer/${idUserCreated}`).field({
-      customerName: 'Agner Functional Test Put',
-      customerEmail: 'agner.functional.test.put@test.com',
-    });
+    const response = await apiRequest
+      .put(`/customer/${idUserCreated}`)
+      .field({
+        customerName: 'Agner Functional Test Put',
+        customerEmail: 'agner.functional.test.put@test.com',
+      })
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
   });
 
   it('should search a specific customer after update and the response be equal 200 and equal specifics object propertys', async () => {
-    const response = await apiRequest.get(`/customer/${idUserCreated}`);
+    const response = await apiRequest
+      .get(`/customer/${idUserCreated}`)
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({
       id: idUserCreated,
@@ -94,17 +121,23 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should search all customer and the response be equal 200', async () => {
-    const response = await apiRequest.get('/customer');
+    const response = await apiRequest
+      .get('/customer')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
   });
 
   it('should search all customers even them deleted and the response be equal 200', async () => {
-    const response = await apiRequest.get('/customer/all-customers');
+    const response = await apiRequest
+      .get('/customer/all-customers')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
   });
 
   it('should searh a specific customer and the response be equal 404 and recive the message customer not found', async () => {
-    const response = await apiRequest.get('/customer/999999999999999');
+    const response = await apiRequest
+      .get('/customer/999999999999999')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
       message: 'Customer with id 999999999999999 not found',
@@ -112,18 +145,18 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should search a specific customer image and the response be equal 200 and equal specifics property', async () => {
-    const response = await apiRequest.get(
-      `/customer/customer-image-buffer/${idUserCreated}`,
-    );
+    const response = await apiRequest
+      .get(`/customer/customer-image-buffer/${idUserCreated}`)
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({ type: 'Buffer' });
     expect(response.body).toHaveProperty('data');
   });
 
   it('should search a specific customer image and the response be equal 404 and recive the message customer not found', async () => {
-    const response = await apiRequest.get(
-      '/customer/customer-image-buffer/999999999999999',
-    );
+    const response = await apiRequest
+      .get('/customer/customer-image-buffer/999999999999999')
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
       message: 'Customer image with id 999999999999999 not found',
@@ -131,7 +164,9 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should delete a specific customer and the response be equal 200', async () => {
-    const response = await apiRequest.delete(`/customer/${idUserCreated}`);
+    const response = await apiRequest
+      .delete(`/customer/${idUserCreated}`)
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(200);
     expect(response.body).toEqual({
       message: 'Customer deleted successfully.',
@@ -139,7 +174,9 @@ describe('Test the customer controller (e2e)', () => {
   });
 
   it('should delete a specific customer and the response be equal 404', async () => {
-    const response = await apiRequest.delete(`/customer/999999999999999`);
+    const response = await apiRequest
+      .delete(`/customer/999999999999999`)
+      .set('Authorization', `Bearer ${accessToken}`);
     expect(response.status).toBe(404);
     expect(response.body).toEqual({
       message: 'Customer with id 999999999999999 not found',
